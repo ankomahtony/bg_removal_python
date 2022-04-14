0031d275b03357ee14fa51edf59d41543edf2a95
Anthony Ankomah <tonyankomah1@gmail.com>
2022-04-14 12:35:41 +0000

    first to deploy

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bg_removal_python
+
+This is a simple streamlit application 
+created with python that takes image and return a removed 
+background png format of the original image
---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from rembg import remove
+import io, os
+from PIL import Image
+
+st.title("Remove the backgroud of your Image")
+image = st.file_uploader('Upload image')
+
+col1, col2 = st.columns(2)
+
+#function
+def remove_bg(image):
+    input_img = Image.open(image)
+    result = remove(input_img)
+    return result
+
+if image:
+    print(image.name)
+
+    filename = image.name.split('.')[:-1]
+    output =''.join(filename)+'_no_bg.png'
+    col1.image(image)
+    new_image = remove_bg(image)
+    col2.image(new_image)
+    new_image.save(output)
+
+    with open(output, "rb") as file:
+
+        btn = col2.download_button(
+
+            label="Download",
+
+            data=file,
+
+            file_name=output,
+
+            mime="image/png"
+
+          )
+        
+        os.remove(output)
+
+    
+
+    # download_image = Image.open()
+    # col2.download_button(label="Download img",data=download_image,file_name=output,mime='image/png' )
