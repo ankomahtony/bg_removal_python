a76d414960f00089f123f8a2ab403ab3d8a73da1
Anthony Ankomah <tonyankomah1@gmail.com>
2022-04-17 12:02:13 +0000

    Set for deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import io, os
 from PIL import Image
 
 st.title("Remove the backgroud of your Image")
-image = st.file_uploader('Upload image')
+image = st.file_uploader('Upload image',type=['png','jpeg','jpg','webp'])
 
 col1, col2 = st.columns(2)
 
@@ -16,14 +16,18 @@ def remove_bg(image):
     return result
 
 if image:
-    print(image.name)
+    
+    st.spinner()
+    with st.spinner(text='Removing the background. Give it a sec...'):
 
-    filename = image.name.split('.')[:-1]
-    output =''.join(filename)+'_no_bg.png'
+        filename = image.name.split('.')[:-1]
+        output =''.join(filename)+'_no_bg.png'
+        new_image = remove_bg(image)
+        new_image.save(output)
+    
+    
     col1.image(image)
-    new_image = remove_bg(image)
     col2.image(new_image)
-    new_image.save(output)
 
     with open(output, "rb") as file:
 
@@ -37,11 +41,14 @@ if image:
 
             mime="image/png"
 
-          )
+        )
         
         os.remove(output)
 
-    
+        
 
+
+
+# st.write("Package: [rembg](https://github.com/danielgatis/rembg)")
     # download_image = Image.open()
     # col2.download_button(label="Download img",data=download_image,file_name=output,mime='image/png' )
